a2452275d2b46e59473cfff5388a7297afb60c9b
SelfishFish07 <nagorskaia.ts@phystech.edu>
2026-02-09 18:12:07 +0000

    fixed lab1

--- Lab№1/Part 2/test.cpp
+++ Lab№1/Part 2/test.cpp
@@ -7,15 +7,19 @@ int main(int argc, char **argv){
     gmsh::initialize();
 
     gmsh::model::add("test");
-    gmsh::merge("../duck.stl");
+    gmsh::merge("../chillguy1.stl");
 
     std::vector<std::pair<int, int> > s;
-    gmsh::model::getEntities(s, 2);
-    double angle = 40; 
-    gmsh::model::mesh::classifySurfaces(angle*M_PI / 180., true, true);
+    gmsh::model::getEntities(s, c:\Users\Тася\Downloads\chillguy1.stl2);
+    double angle = 70; 
+    gmsh::model::mesh::classifySurfaces(angle * M_PI / 180., true, true);
     gmsh::model::mesh::createGeometry();
 
+    gmsh::option::setNumber("Mesh.Algorithm", 6);
     gmsh::option::setNumber("Mesh.MeshSizeMax", 2.0); 
+    gmsh::option::setNumber("Mesh.MeshSizeMin", 1.0);
+    gmsh::option::setNumber("Mesh.MeshSizeFromCurvature", 0);
+
 
     gmsh::model::getEntities(s, 2);
     std::vector<int> sl;
