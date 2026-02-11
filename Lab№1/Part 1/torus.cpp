5f4504536c44843bc7530e93c932b6891a025851
SelfishFish07 <nagorskaia.ts@phystech.edu>
2026-02-11 08:54:35 +0000

    fixed lab1

--- Lab№1/Part 1/torus.cpp
+++ Lab№1/Part 1/torus.cpp
@@ -1,17 +1,25 @@
-#include <set>
 #include <gmsh.h>
+#include <vector>
+#include <utility>
+#include <set>
 
 int main(int argc, char **argv){
     gmsh::initialize();
     gmsh::model::add("torus");
 
-    gmsh::model::occ::addTorus(0, 0, 0, 5, 2.5);
+    gmsh::model::occ::addTorus(0, 0, 0, 5, 2, 1);
+    gmsh::model::occ::addTorus(0, 0, 0, 5, 1, 2);
+
+    std::vector<std::pair<int, int> > ov;
+    std::vector<std::vector<std::pair<int, int> > > ovv;
+    gmsh::model::occ::cut({{3, 1}}, {{3, 2}}, ov, ovv, 3);
+
 
     gmsh::model::occ::synchronize();
 
-    gmsh::option::setNumber("Mesh.MeshSizeMax", 1);
+    gmsh::option::setNumber("Mesh.MeshSizeMax", 0.5);
     
-    gmsh::model::mesh::generate(2);
+    gmsh::model::mesh::generate(3);
 
     gmsh::write("torus.msh");
 
